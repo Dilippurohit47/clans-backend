5033ecd3effa2b13f320e384e5ec2197a9c1088d
Dilippurohit47 <dilippurohit204@gmail.com>
2024-10-02 16:09:27 +0530

    initial project

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+app.get("/", (req, res) => {
+    res.render("welcome");
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from "express";
+import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app: Application = express();
+
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("welcome");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
