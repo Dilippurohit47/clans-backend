0ac8df19ab3bad9be5ead1f064e51c606c914907
Dilippurohit47 <dilippurohit204@gmail.com>
2024-10-04 11:58:55 +0530

    user creation api

--- dist/routes/authRoutes.js
+++ dist/routes/authRoutes.js
@@ -3,20 +3,35 @@ import { registerSchema } from "../validations/authValidatons.js";
 import { formatError } from "../helper/errorformat.js";
 import { ZodError } from "zod";
 import prisma from "../config/database.js";
+import bcrypt, { genSalt } from "bcrypt";
 const router = Router();
 router.post("/", async (req, res) => {
     const body = req.body;
     try {
         const paylaod = registerSchema.parse(body);
-        let user = await prisma.user.findUnique({ where: {
-                email: paylaod.email
-            } });
+        let user = await prisma.user.findUnique({
+            where: {
+                email: paylaod.email,
+            },
+        });
         if (user) {
-            return res.json(422).json({ erros: {
-                    email: "Eamil already taken Please use another one "
-                } });
+            return res.json(422).json({
+                erros: {
+                    email: "Eamil already taken Please use another one ",
+                },
+            });
         }
-        return res.json(paylaod);
+        const salt = await genSalt(10);
+        paylaod.password = await bcrypt.hash(paylaod.password, salt);
+        await prisma.user.create({
+            data: {
+                name: paylaod.name,
+                email: paylaod.email,
+                password: paylaod.password,
+            },
+        });
+        console.log("user created succesfully");
+        return res.json("user created successfully");
     }
     catch (error) {
         if (error instanceof ZodError) {
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,7 +3,7 @@ import { registerSchema } from "../validations/authValidatons.js";
 import { formatError } from "../helper/errorformat.js";
 import { ZodError } from "zod";
 import prisma from "../config/database.js";
-
+import bcrypt, { genSalt } from "bcrypt";
 const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
@@ -12,18 +12,32 @@ router.post("/", async (req: Request, res: Response) => {
   try {
     const paylaod = registerSchema.parse(body);
 
-    let user  = await prisma.user.findUnique({where:{
-        email:paylaod.email
-    }})
-    if(user) {
-        return res.json(422).json({erros:{
-            email :"Eamil already taken Please use another one "
-        }})
+    let user = await prisma.user.findUnique({
+      where: {
+        email: paylaod.email,
+      },
+    });
+    if (user) {
+      return res.json(422).json({
+        erros: {
+          email: "Eamil already taken Please use another one ",
+        },
+      });
     }
 
-    
+    const salt = await genSalt(10);
 
-    return res.json(paylaod);
+    paylaod.password = await bcrypt.hash(paylaod.password, salt);
+
+    await prisma.user.create({
+      data: {
+        name: paylaod.name,
+        email: paylaod.email,
+        password: paylaod.password,
+      },
+    });
+    console.log("user created succesfully");
+    return res.json("user created successfully");
   } catch (error) {
     if (error instanceof ZodError) {
       const errors = formatError(error);
